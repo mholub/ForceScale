44775fb162905eb1e6410b5c9b540d1da5a9a548
Michael Holub <michael.holub@gmail.com>
2015-03-21 02:42:37 +0300

    Initial commit

---
+++ ForceScale/Scripts/ForceScaleEditorWindow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace MH.ForceScale {
+public class ForceScaleEditorWindow : EditorWindow {
+	[MenuItem("Assets/Force Scale Settings")]
+	public static void OpenWindow() {
+		EditorWindow.GetWindow<ForceScaleEditorWindow>().Show();
+	}
+
+	void OnGUI() {
+		GUILayout.BeginVertical();
+		ForceScaleForAllModelsInFolder.GlobalScale = EditorGUILayout.FloatField("Global scale", ForceScaleForAllModelsInFolder.GlobalScale);
+		EditorGUILayout.Space();
+		ForceScaleForAllModelsInFolder.ForceScaleFolderName = EditorGUILayout.TextField("Path should contain", ForceScaleForAllModelsInFolder.ForceScaleFolderName);
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("You need to reimport assets after settings changed");
+		GUILayout.EndVertical();
+	}
+}
+}
---
+++ ForceScale/Scripts/ForceScaleForAllModelsInFolder.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace MH.ForceScale {
+  [InitializeOnLoad]
+  public class ForceScaleForAllModelsInFolder : UnityEditor.AssetPostprocessor {
+    private static string _forceScaleFolderName;
+    private static float _globalScale;
+    
+    public static string ForceScaleFolderName {
+      get {
+        return _forceScaleFolderName;
+      }
+      set {
+        if (_forceScaleFolderName != value) {
+          _forceScaleFolderName = value;
+          EditorPrefs.SetString("ForceScaleForAllModelsInFolder.ForceScaleFolderName", ForceScaleFolderName);
+        }
+      }
+    }
+    public static float GlobalScale {
+      get {
+        return _globalScale;
+      }
+      set {
+        if (_globalScale != value) {
+          _globalScale = value;
+          EditorPrefs.SetFloat("ForceScaleForAllModelsInFolder.GlobalScale", GlobalScale);
+        }
+      }
+    }
+    
+    static ForceScaleForAllModelsInFolder() {
+      Debug.Log("init");
+      ForceScaleFolderName = EditorPrefs.GetString("ForceScaleForAllModelsInFolder.ForceScaleFolderName", "ForceScale");
+      GlobalScale = EditorPrefs.GetFloat("ForceScaleForAllModelsInFolder.GlobalScale", 1);
+    }
+    
+    void OnPreprocessModel() {
+      if (assetPath.Contains(ForceScaleFolderName)) {
+        Debug.LogFormat("{0} global scale was changed to {1}", assetPath, GlobalScale);
+        var modelImporter = assetImporter as ModelImporter;
+        modelImporter.globalScale = GlobalScale;
+      }
+    }
+  }
+}
